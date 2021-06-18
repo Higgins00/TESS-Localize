7e25364a67db5575e9b1d6238271b0541cf4ecbf
Higgins00 <43622895+Higgins00@users.noreply.github.com>
2021-06-18 05:16:32 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup
+setup(name='Testing_Disentangler_Draft',
+version='0.1',
+description='Testing of Package',
+url='#',
+author='Michael Higgns',
+author_email='michael.higgins@duke.edi',
+license='MIT',
+packages=['Disentangler_Draft'],
+zip_safe=False)
+

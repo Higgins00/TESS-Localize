08de51c3b838b292a6cccfc3d3454f3f5e736d2b
Higgins00 <43622895+Higgins00@users.noreply.github.com>
2021-07-07 17:23:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt", "r") as fh:
 setup(name='TESS_Localize',
 version='0.1',
 description='Package for localizing variable stars in TESS Photometry',
-url='#',
+url='https://github.com/Higgins00/TESS-Localize',
 author='Michael Higgins',
 author_email='michael.higgins@duke.edu',
 license='GNU General Public License v3.0',
